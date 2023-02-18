fbc5a2f7858ca59e6bd97f4174e64e03801984c7
rashedrk <rashedrk001@gmail.com>
2023-02-18 15:34:21 +0600

    displayArea funtion added to display area into area calculation card, calculation area is functional for first card(triangle)

--- js/index.js
+++ js/index.js
@@ -11,17 +11,47 @@ function getInputFieldValueById(inputId) {
     return inputValue
 }
 
-
-document.getElementById('triangle-calculate-btn').addEventListener('click', function () {
-    // get input value 
-    const b = getInputFieldValueById('triangle-first-input');
-    const h = getInputFieldValueById('triangle-second-input');
-
+// function to multiply two input 
+function multiplyInputs(firstInputId,secondInputId) {
+    // get input value
+    const firstInput = getInputFieldValueById(firstInputId);
+    const secondInput = getInputFieldValueById(secondInputId);
     // validation part 2
-    if (b == false || h == false) {
-        return alert('please enter a valid number')
+    if (firstInput == false || secondInput == false) {
+        return alert('please enter a valid number');
     }
+    // return result after multiply
+    return firstInput * secondInput
+}
+
+// function to display area after calculation
+function displayArea(serial,name,area) {
+    const tableContainer = document.getElementById('table-container');
+    const tr = document.createElement('tr')
+    tr.innerHTML = `
+        <th>${serial}</th>
+        <td>${name}</td>
+        <td>${area}cm<sup>2<sup></td>
+        <td><button class="btn btn-primary">Convert to m<sup>2</sup></button></td>
+        
+    `;
+    tableContainer.appendChild(tr);
+}
+let serial=0;
+document.getElementById('triangle-calculate-btn').addEventListener('click', function () {
+    // multiply two inputs
+    // b x h
+    const inputMultiply = multiplyInputs('triangle-first-input', 'triangle-second-input');
+
     // calculate the area 
-    const area = 0.5 * b *h;
-    console.log(area);
+    // area = 0.5 x b x h
+    const area = 0.5 * inputMultiply;
+
+    // display area to area calculation card 
+    // return if input is invalid
+    if(isNaN(area)){
+        return
+    }
+    serial += 1;
+    displayArea(serial,'Triangle',area);
 })
